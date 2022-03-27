23304be37024cb5b822eb527a1acf80f320fa93f
Tony Vf <tonyvf@Tony-MacBook-Air.local>
2022-03-27 19:14:16 +0200

    update

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,63 +1,95 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router" target="_blank" rel="noopener">router</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-typescript" target="_blank" rel="noopener">typescript</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <v-container class="main text-center">
+    <h1 class="title">To-do list</h1>
+    <form class="form">
+      <v-text-field
+        label="Input your next task"
+        hide-details="auto"
+        v-model="todo"
+        v-on:keydown.enter.prevent="addTasks"
+      ></v-text-field>
+    </form>
+
+    <section class="items">
+      <div v-for="task in tasks" :key="task" class="items-list">
+        <p>{{ task.text }}</p>
+        <v-btn class="items-button" @click="finish(task.id, task.text)"
+          >Done</v-btn
+        >
+      </div>
+    </section>
+
+    <section class="items">
+    <v-btn v-if="!showFinished" @click="showFinished = !showFinished">Show finished</v-btn>
+    <v-btn v-else @click="showFinished = !showFinished">hide finished</v-btn> 
+    <!-- existuje alternativa použitím jen 1 tlačítka? -->
+    <div v-if="showFinished">
+      <div v-for="task in finished" :key="task" class="items-list">
+        <p>{{ task.text }}</p>
+      </div>
+    </div>
+    </section>
+  </v-container>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent } from "vue";
 
 export default defineComponent({
-  name: 'HelloWorld',
-  props: {
-    msg: String,
+  name: "HelloWorld",
+
+  data() {
+    return {
+      todo: "",
+      showFinished: false,
+      tasks: [
+        { id: 1, text: "buy milk" },
+        { id: 2, text: "call boss" }, 
+      ],
+      finished: [{ id: 0, text: "" }], 
+      //neviditelný prvek array, 
+      //potřebuju zjistit jak se předefinuje array bez obsahu
+      //mám na mysli například prázdný array s kontrukcí [{id: number, text: string}]
+    };
   },
+
+  methods: {
+    addTasks() {
+      this.tasks.push({ id: this.tasks.length + 1, text: this.todo });
+      this.todo = "";
+    },
+    finish(taskId: number, taskText: string) {
+      this.finished.push({ id: this.finished.length + 1, text: taskText });
+      this.tasks = this.tasks.filter((obj) => {
+        return obj.id !== taskId;
+      });
+    },
+  },
+  //možná sáhnout si více po lifecyklu, nejsem si u nich moc jistý
 });
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
+<style lang="scss" scoped>
+.main {
+  display: relative;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+.form {
+  display: flex;
+  flex-flow: column;
+  max-width: 50%;
+  margin: auto auto;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+.items {
+  margin-bottom: 50px;
+  &-list {
+    display: flex;
+    flex-flow: row;
+    align-items: center;
+    justify-content: center;
+    padding: 10px;
+  }
+  &-button {
+    margin-left: 10px;
+  }
 }
 </style>
